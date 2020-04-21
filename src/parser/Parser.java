91de6098c50baf6d40009fa6f4419555ecce237f
akiyoss-git <58162197+akiyoss-git@users.noreply.github.com>
2020-04-21 14:20:08 +0300

    Unlimited text file

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -9,69 +9,48 @@ public class Parser {
     }
 
     public Characters[] parse(){
-        Characters[] mass = new Characters[5];
         try
         {
             FileReader reader = new FileReader(this.filename);
             BufferedReader breader = new BufferedReader(reader);
-            String name;
-            String type;
+            StringBuilder sb = new StringBuilder();
             int i = 0;
             String line = breader.readLine();
             while (line != null){
-                String[] data = new String[4];
-                int j = 0;
-                for(String retval : line.split(" ")){
-                    data[j] = retval;
-                    j = j+1;
-                }
-                if(data[1].equals("tank")){
-                    Characters chars = new Tank(data[3]);
-                    mass[i] = chars;
-                } else {
-                    if(data[1].equals("melee")){
-                        Characters chars = new Melee(data[3]);
-                        mass[i] = chars;
-                    } else {
-                        if(data[1].equals("range")){
-                            Characters chars = new Range(data[3]);
-                            mass[i] = chars;
-                        } else{
-                            mass[i] = null;
-                        }
-                    }
-                }
-
+                sb.append(line + ' ');
                 line = breader.readLine();
                 i = i + 1;
             }
+            Characters[] mass = new Characters[i];
+            String data = sb.toString();
+            String[] md = data.split(" ");
+            for (int j = 0; j < i; j++) {
+                if(md[1 + j * 4].equals("tank")){
+                    Characters chars = new Tank(md[3 + j * 4]);
+                    mass[j] = chars;
+                } else {
+                    if (md[1 + j * 4].equals("melee")) {
+                        Characters chars = new Melee(md[3 + j * 4]);
+                        mass[j] = chars;
+                    } else {
+                        if (md[1 + j * 4].equals("range")) {
+                            Characters chars = new Range(md[3 + j * 4]);
+                            mass[j] = chars;
+                        } else {
+                            mass[j] = null;
+                        }
+                    }
+                }
+            }
+
             return mass;
 
         }catch (FileNotFoundException e) {
             e.printStackTrace();
+            return null;
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        return mass;
-    }
-
-    public static void write() {
-
-        try(FileWriter writer = new FileWriter("notes3.txt", false))
-        {
-            // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-
-            writer.flush();
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-
+            return null;
         }
     }
 }
